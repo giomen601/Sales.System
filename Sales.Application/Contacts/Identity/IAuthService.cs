a9a9c36b6184cc07ff529492b8683c52f29b35f7
giomen601 <arizamendozagiovany@gmail.com>
2023-05-15 22:56:10 -0500

    auth-initial

---
+++ Sales.Application/Contacts/Identity/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Sales.Application.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Application.Contacts.Identity;
+public interface IAuthService
+{
+  Task<AuthResponse> Login(AuthRequest request);
+  Task<RegistrationResponse> Register(RegistrationRequest request);
+}
